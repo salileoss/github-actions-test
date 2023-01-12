31958909e44edc064d9361229e843e026596eedd
salileoss <lisalileo@gmail.com>
2023-01-12 15:40:55 -0800

    revert changes

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ window.onload = () => {
   const game = new Game(p1, p2)
   const turn = document.getElementById('turn')
   const player = document.getElementById('player')
-  // some changes
-  // some other changes
   player.innerText = game.player
 
   document.querySelectorAll('td').forEach((el) => {
